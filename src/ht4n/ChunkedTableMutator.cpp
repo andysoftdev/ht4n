1f12bbfc862ee2070fa808ab0d8fd00edd122412
andy <andy.softdev@bluewin.ch>
2012-02-07 23:54:14 +0100

    License updated to GPL v3

--- src/ht4n/ChunkedTableMutator.cpp
+++ src/ht4n/ChunkedTableMutator.cpp
@@ -5,7 +5,7 @@
  *
  * ht4n is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
+ * as published by the Free Software Foundation; either version 3
  * of the License, or any later version.
  *
  * Hypertable is distributed in the hope that it will be useful,
