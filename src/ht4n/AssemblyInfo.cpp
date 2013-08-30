0b4d11be7af95ea332c5cf787ca1012fcbb7aa2b
andy <andy.softdev@bluewin.ch>
2013-08-30 11:59:52 +0200

    Version updated to 0.9.7.10

--- src/ht4n/AssemblyInfo.cpp
+++ src/ht4n/AssemblyInfo.cpp
@@ -36,8 +36,8 @@ using namespace System::Security::Permissions;
 [assembly: AssemblyDescription("Hypertable .NET client library")];
 [assembly: AssemblyCompany("ht4n.softdev.ch")];
 [assembly: AssemblyCopyright("Copyright © 2010-2013")];
-[assembly: AssemblyVersion("0.9.7.9")];
-[assembly: AssemblyFileVersion("0.9.7.9")];
+[assembly: AssemblyVersion("0.9.7.10")];
+[assembly: AssemblyFileVersion("0.9.7.10")];
 
 [assembly: CLSCompliant(true)];
 [assembly: ComVisible(false)];
