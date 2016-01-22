8477519d4c4d78f55c4d22edf2955ee98e30f369
andy <andy.softdev@bluewin.ch>
2016-01-22 16:32:02 +0100

    Copyright have been updated

--- src/ht4n.Test/TestHql.cs
+++ src/ht4n.Test/TestHql.cs
@@ -1,5 +1,5 @@
 ﻿/** -*- C# -*-
- * Copyright (C) 2010-2015 Thalmann Software & Consulting, http://www.softdev.ch
+ * Copyright (C) 2010-2016 Thalmann Software & Consulting, http://www.softdev.ch
  *
  * This file is part of ht4n.
  *
