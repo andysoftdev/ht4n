3fdf8676de3321614fd2f0ce160bcff10c2296ea
andy <andy.softdev@bluewin.ch>
2022-10-14 08:58:35 +0200

    Remove tss cleanup

--- src/ht4n/DllMain.cpp
+++ src/ht4n/DllMain.cpp
@@ -127,22 +127,3 @@ extern "C" BOOL WINAPI DllMain(HANDLE /*hInstance*/, DWORD dwReason, LPVOID /*lp
 
 	return true;
 }
-
-namespace boost
-{
-	void tss_cleanup_implemented(void) {
-		/*
-		This function's sole purpose is to cause a link error in cases where
-		automatic tss cleanup is not implemented by Boost.Threads as a
-		reminder that user code is responsible for calling the necessary
-		functions at the appropriate times (and for implementing an a
-		tss_cleanup_implemented() function to eliminate the linker's
-		missing symbol error).
-
-		If Boost.Threads later implements automatic tss cleanup in cases
-		where it currently doesn't (which is the plan), the duplicate
-		symbol error will warn the user that their custom solution is no
-		longer needed and can be removed.
-		*/
-	}
-}
